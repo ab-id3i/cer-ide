b0500f0e03e8e09535829be0be1e80a472b076f8
amau_pro <amaury.betaouaf@id3i.fr>
2025-06-08 17:49:09 +0200

    Update MonacoEditor.vue

--- frontend/src/components/MonacoEditor.vue
+++ frontend/src/components/MonacoEditor.vue
@@ -47,7 +47,7 @@ const sendContentChange = debounce((value: any) => {
       timestamp: Date.now()
     });
   }
-}, 1000); // envoie toutes les 1000ms max
+}, 100); // envoie toutes les 1000ms max
 
 // Ajout du debounce pour la gestion des positions du curseur
 const debouncedCursorPositionChange = debounce((position: any) => {
@@ -58,7 +58,7 @@ const debouncedCursorPositionChange = debounce((position: any) => {
       position
     });
   }
-}, 1000); // 1000ms de debounce
+}, 100); // 1000ms de debounce
 
 onMounted(() => {
   const requireScript = document.createElement('script');
